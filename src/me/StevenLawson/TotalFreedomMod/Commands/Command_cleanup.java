24a0064c0f5c13e427ac5001e60c549602b058cb
Ryan <ryan.ictclubsales@gmail.com>
2013-10-29 23:41:02 +0000

    Command cleanups

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_cleanup.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_cleanup.java
@@ -7,24 +7,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.ONLY_CONSOLE)
-@CommandParameters(description = "Runs the cleanup system.", usage = "/<command>")
-public class Command_mcmyadmin extends TFM_Command
+@CommandParameters(description = "Runs the servers cleanup system.", usage = "/<command>")
+public class Command_cleanup extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.bcastMsg(ChatColor.RED + "Starting Midnightly Clean Up - Expect Lag");
+        TFM_Util.bcastMsg(ChatColor.RED + "Server Cleanup System Starting");
 
         if (senderIsConsole)
         {
-            server.dispatchCommand(sender, "nonuke off");
             server.dispatchCommand(sender, "opall");
-            server.dispatchCommand(sender, "clearall");
-            server.dispatchCommand(sender, "backup");
             server.dispatchCommand(sender, "tfipbanlist purge");
             server.dispatchCommand(sender, "tfbanlist purge");
-        TFM_Util.bcastMsg(ChatColor.GREEN + "Midnightly Clean Up Completed. Reloading Server");
-            server.dispatchCommand(sender, "reload");
+        TFM_Util.bcastMsg(ChatColor.GREEN + "Server Cleanup System Finished!");
         }
 
         return true;
