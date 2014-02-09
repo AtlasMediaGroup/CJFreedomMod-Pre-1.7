4051e883f730dc956a9eef38d8c0cb5cab70b778
Paldiu <officialzimphony@gmail.com>
2014-02-09 01:58:12 +0000

    Fixed dynamic range ban to single ip ban.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
@@ -80,7 +80,7 @@ public class Command_gtfo extends TFM_Command
         String[] ip_parts = user_ip.split("\\.");
         if (ip_parts.length == 4)
         {
-            user_ip = String.format("%s.%s.*.*", ip_parts[0], ip_parts[1]);
+            user_ip = String.format("%s.%s.%s.%s", ip_parts[0], ip_parts[1], ip_parts[2], ip_parts[3]);
         }
         TFM_Util.bcastMsg(String.format("%s - banning: %s, IP: %s for '%s'.", sender.getName(), player.getName(), user_ip, ban_reason), ChatColor.RED);
         TFM_ServerInterface.banIP(user_ip, ban_reason, null, null);
